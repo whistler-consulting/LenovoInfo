02de4b56e0830ecbe3116f41ad51fecfdeeeeb9b
mjenne <mjenne@whistler-consulting.com>
2017-02-10 17:51:44 -0600

    working sample

--- LenovoInfo.psm1
+++ LenovoInfo.psm1
@@ -1,5 +1,42 @@
 ﻿Set-StrictMode -Version Latest
 
+Function Invoke-LenovoWarrantyRESTAPI {
+[CmdletBinding()]
+
+    param
+    (
+        $Serial,
+        $Product
+    )
+
+    $url = "https://ibase.lenovo.com/POIRequest.aspx"
+    $verb = "POST"
+
+    $header = @{ "Content-Type" = "application/x-www-form-urlencoded" }
+
+    $body = [xml]"<wiInputForm source='ibase'><id>LSC3</id><pw>IBA4LSC3</pw><product></product><serial></serial><wiOptions><machine/><parts/><service/><upma/><entitle/></wiOptions></wiInputForm>"
+
+    if (!([string]::IsNullOrEmpty($Product)))
+    {
+        #product parameter was provided
+
+        $node = $body.SelectSingleNode("//product")
+        $node.InnerText = $Product
+    }
+
+    if (!([string]::IsNullOrEmpty($SerialNumber)))
+    {
+        #model parameter was provided
+
+        $node = $body.SelectSingleNode("//serial")
+        $node.InnerText = $SerialNumber
+    }
+
+    $response = Invoke-RestMethod -Method $verb -Uri $url -Body ("xml=$($body.InnerXml))") -Headers $header
+
+    return $response
+}
+
 
 Function Get-LenovoWarrantyInfo {
 [CmdletBinding(DefaultParameterSetName="ByComputerName")]
@@ -13,7 +50,7 @@ Function Get-LenovoWarrantyInfo {
         Position = 0,
         ParameterSetName="BySerialNumber")]
         [Alias('SN')]
-        $SerialNumber,
+        [string]$SerialNumber,
 
         #Lenovo Product Code for device being queried
         [Parameter(Mandatory = $false,
@@ -21,49 +58,31 @@ Function Get-LenovoWarrantyInfo {
         ValueFromPipeline = $true,
         Position = 1,
         ParameterSetName = "BySerialNumber")]
-        $Product,
+        [string]$Product,
 
         [Parameter(Mandatory = $false,
         ValueFromPipelineByPropertyName = $true,
         ValueFromPipeline = $true,
         Position = 0,
         ParameterSetName = "ByComputerName")]
-        $ComputerName,
+        [string]$ComputerName,
 
         [Parameter(Mandatory = $false)]
-        $Credential
+        [System.Management.Automation.CredentialAttribute()]$Credential
         
     )
-
-    $url = "https://ibase.lenovo.com/POIRequest.aspx"
-    $verb = "POST"
-
-    $header = @{ "Content-Type" = "application/x-www-form-urlencoded" }
-
-    $body = [xml]"<wiInputForm source='ibase'><id>LSC3</id><pw>IBA4LSC3</pw><product></product><serial></serial><wiOptions><machine/><parts/><service/><upma/><entitle/></wiOptions></wiInputForm>"
 
     switch($PsCmdlet.ParameterSetName)
     {
         "BySerialNumber"
         {
-            if (!([string]::IsNullOrEmpty($Product)))
-            {
-                #product parameter was provided
-
-                $node = $body.SelectSingleNode("//product")
-                $node.InnerText = $Product
-            }
-
-            if (!([string]::IsNullOrEmpty($SerialNumber)))
-            {
-                #model parameter was provided
-
-                $node = $body.SelectSingleNode("//serial")
-                $node.InnerText = $SerialNumber
-            }
+            #don't need anything extra because we already have serial number and possibly product code
+        }
         }
         "ByComputerName"
         {
+            # need to use WMI to lookup serial number and model number of device
+            
             $args = @{ namespace = "root\cimv2"
                         class = "Win32_SystemEnclosure"
                         ErrorAction = "SilentlyContinue"}
@@ -97,65 +116,63 @@ Function Get-LenovoWarrantyInfo {
                 $Product = $result | Select-Object -ExpandProperty Model
 
                 $Manufacturer = $result | Select-Object -ExpandProperty Manufacturer
+
+                if ($Manufacturer -notmatch "Lenovo")
+                {
+                    Write-Warning "Possibly not a Lenovo device"
+                }
             }
         }
 
+    $args = @{}
+    $args.Add("Serial", $SerialNumber)
 
+    if ($null -ne $Product) { $args.Add("Product", $Product) }
+
+    $webresponse = Invoke-LenovoWarrantyRESTAPI @args
+
+    if ($null -eq $webresponse) 
+    {
+        Write-Error "REST API returned NULL"
     }
+    else
+    {
+        #REST API returned data
+
+        $properties = @{}
+
+        if ($webresponse.SelectSingleNode("serviceInfo")) {
+
+            $properties.Add("ShipDate", $webresponse.wiOutputForm.warrantyInfo.serviceInfo.shipDate[0])
+            $properties.Add("WarrantyStartDate", $webresponse.wiOutputForm.warrantyInfo.serviceInfo.warstart[0])
+            $properties.Add("WarrantyEndDate", $webresponse.wiOutputForm.warrantyInfo.serviceInfo.wed[0])
+        } else {
+            $properties.Add("ShipDate", $null)
+            $properties.Add("WarrantyStartDate", $null)
+            $properties.Add("WarrantyEndDate", $null)
+        }
 
 
+        if ($webresponse.SelectSingleNode("machineInfo")) {
 
-    $SerialNumber
-    $Product
-    $ComputerName
-    $Manufacturer
+            $properties.Add("Model", $webresponse.wiOutputForm.warrantyInfo.machineinfo.model)
+            $properties.Add("Type", $webresponse.wiOutputForm.warrantyInfo.machineinfo.type)
+            $properties.Add("Product", $webresponse.wiOutputForm.warrantyInfo.machineinfo.product)
+            $properties.Add("SerialNumber", $webresponse.wiOutputForm.warrantyInfo.machineinfo.serial)
+        }
 
-    break
+        $errorMessage = $webresponse.wiInputForm.xmlMessages.xmlMessage | Where-Object {$_.type -eq "error"}
 
-   
+        if ( $errorMessage -ne $null  ) {
+            $properties.Add("ErrorNumber",($errorMessage.num))
+            $properties.Add("ErrorText", ($errorMessage.InnerText))
 
-
-
-
-    $webresponse = Invoke-RestMethod -Method $verb -Uri $url -Body $body -Headers $header
-
-    $properties = @{}
-
-    $properties.Add("LastUpdate", (get-date))
-
-    if ($webresponse.wiOutputForm.warrantyInfo.serviceInfo) {
-
-        $properties.Add("ShipDate", $webresponse.wiOutputForm.warrantyInfo.serviceInfo.shipDate[0])
-        $properties.Add("WarrantyStartDate", $webresponse.wiOutputForm.warrantyInfo.serviceInfo.warstart[0])
-        $properties.Add("WarrantyEndDate", $webresponse.wiOutputForm.warrantyInfo.serviceInfo.wed[0])
-    } else {
-        $properties.Add("ShipDate", $null)
-        $properties.Add("WarrantyStartDate", $null)
-        $properties.Add("WarrantyEndDate", $null)
-    }
-
-
-    if ($webresponse.wiOutputForm.warrantyInfo.machineinfo) {
-
-        $properties.Add("Model", $webresponse.wiOutputForm.warrantyInfo.machineinfo.model)
-        $properties.Add("Type", $webresponse.wiOutputForm.warrantyInfo.machineinfo.type)
-        $properties.Add("Product", $webresponse.wiOutputForm.warrantyInfo.machineinfo.product)
-        $properties.Add("SerialNumber", $webresponse.wiOutputForm.warrantyInfo.machineinfo.serial)
-    }
-
-    $errorMessage = $webresponse.wiInputForm.xmlMessages.xmlMessage | Where-Object {$_.type -eq "error"}
-
-    if ( $errorMessage -ne $null  ) {
-        $properties.Add("ErrorNumber",($errorMessage.num))
-        $properties.Add("ErrorText", ($errorMessage.InnerText))
-
+        }
     }
 
     $WarrantyInfo = New-Object –TypeName PSObject –Prop $properties
 
-
     $WarrantyInfo    
-
 }
 
 Export-ModuleMember Get-LenovoWarrantyInfo
